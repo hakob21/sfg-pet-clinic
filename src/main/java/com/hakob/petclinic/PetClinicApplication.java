74c4feb60b001084fb85681cf246f647cd7a1ebb
hakob21 <hakob21@gmail.com>
2020-02-14 04:32:27 +0100

    adding POJOs. Closes #2

--- src/main/java/com/hakob/petclinic/PetClinicApplication.java
+++ src/main/java/com/hakob/petclinic/PetClinicApplication.java
@@ -1,7 +1,7 @@
 package com.hakob.petclinic;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PetClinicApplication {
---
+++ src/main/java/com/hakob/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.hakob.petclinic.model;
+
+public class Vet extends Person {
+}
